9407da5a611455bf0fc2f5d58ee2bd91d57cc494
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-03-21 20:11:39 -0500

    Loops for while

---
+++ KeepCoding_PythonDesdeCero/704_loops_while.py
@@ -0,0 +1,9 @@
+totalSum = 0
+myCounter = 1
+
+# While Loop
+while myCounter <= 100:
+    totalSum += myCounter
+    myCounter +=1
+print(totalSum)
+
---
+++ KeepCoding_PythonDesdeCero/705_loops_for_range.py
@@ -0,0 +1,9 @@
+totalSum = 0
+myCounter = 1
+
+# for Loop
+for myCounter in range(1,101):  # range() excludes top limit
+    totalSum += myCounter
+    myCounter +=1
+print(totalSum)
+
