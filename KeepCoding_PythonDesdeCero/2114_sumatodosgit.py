27c653faedccc433207c88498e513b6bd5c9625d
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-05-16 12:25:58 -0500

    Funciones de Primer Nivel

--- KeepCoding_PythonDesdeCero/2114_sumatodosgit.py
+++ KeepCoding_PythonDesdeCero/2114_sumatodosgit.py
@@ -4,4 +4,11 @@ def sumaTodos(limitTo):
         resultados += i
     return resultados
 
+def sumaTodosLosCuadrados(limitTo):
+    resultados = 0
+    for i in range (1,limitTo+1):
+        resultados += i * i
+    return resultados
+
 print(sumaTodos(5))
+print(sumaTodosLosCuadrados(3))
