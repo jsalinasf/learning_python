8eb1b3633faacdf863ecc408e67be8250790293a
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-05-16 12:31:06 -0500

    Funciones como Ciudadanos de Primer Nivel en Python

---
+++ KeepCoding_PythonDesdeCero/2121_funcionescomoparametros.py
@@ -0,0 +1,14 @@
+def normal(x):
+    return x
+
+def cuadrados(x):
+    return x * x
+
+def sumaTodos(limitTo, myfunction):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado += myfunction(i)
+    return resultado
+
+print(sumaTodos(5, normal))
+print(sumaTodos(3, cuadrados))
