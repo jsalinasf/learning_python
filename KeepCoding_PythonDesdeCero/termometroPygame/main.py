a2e8808bb174ae327bd66cf43ae657d72c35946c
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-05-20 17:48:05 -0500

    Imagenes Termometro Pygame

--- KeepCoding_PythonDesdeCero/termometroPygame/main.py
+++ KeepCoding_PythonDesdeCero/termometroPygame/main.py
@@ -1,6 +1,10 @@
 import pygame, sys
 from pygame.locals import *
 
+class Termomentro():
+    def __init__(self):
+        self.custome = pygame.image.load('images/termo1.png')
+
 class mainApp():
     termometro = None
     entrada = None
@@ -10,17 +14,21 @@ class mainApp():
         self.__screen = pygame.display.set_mode((290, 415))
         pygame.display.set_caption('Termometro')
         self.__screen.fill((244, 236, 203))
+        self.termometro = Termomentro()
     
     def __on_close(self):
         pygame.quit()
         sys.exit()
     
     def start(self):
-        while True:
+        exitApp = False
+        while not exitApp:
             for event in pygame.event.get():
                 if event.type == QUIT:
+                    exitApp = True
                     self.__on_close
             
+            self.__screen.blit(self.termometro.custome, (50, 34))
             pygame.display.flip()
 
 if __name__ == '__main__':
