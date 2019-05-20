81a11bb473c9eb74a1b0461b7c11cab2bcb52ead
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-05-19 21:59:38 -0500

    Carrera Pygame - Crear Screen, Background y Bucle principal

--- KeepCoding_PythonDesdeCero/carreraPygame/main.py
+++ KeepCoding_PythonDesdeCero/carreraPygame/main.py
@@ -1,8 +1,29 @@
 import pygame
+import sys
 
 class Game():
     
     corredores = []
 
     def __init__(self):
-        pass
+        self.__screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption('Carrera de Bichos')
+        self.background = pygame.image.load('images/background.png')
+
+    def competir(self):
+
+        while True:
+            # comprobar eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            # refrescar y renderizar pantalla
+            self.__screen.blit(self.background, (0, 0))
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    game.competir()
